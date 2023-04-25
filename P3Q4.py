1a5a2e0e591297270feb86ab0fb236a422941a46
Chaps42 <84743383+Chaps42@users.noreply.github.com>
2023-04-24 22:53:35 -0600

    Update P3Q4.py
    Full functionality for P3Q4. Able to calculate horizon for any pairs of points

--- P3Q4.py
+++ P3Q4.py
@@ -19,19 +19,9 @@ def main():
     # Load the images
     img1 = cv2.imread(args.src_image)
 
-    # Copy images to another variable
-    img1_c = img1.copy()
-
-    # Find height and width of the original image
-    height, width, channels = img1.shape
-
-
     src_pts = []
-    dst_pts = []
     slopes = []
     intercepts = []
-    HorizonSlope = 0
-    HorizonIntercept = 0
 
     def click_event_image_1 (event, x, y, flags, params):  
         if event == cv2.EVENT_LBUTTONDOWN:
@@ -49,7 +39,7 @@ def main():
     cv2.destroyAllWindows()   
 
     #Creates a new image of the two images with lines drawn in between the points
-    for i in range(len(src_pts)/2):
+    for i in range(4):
         src_pts[2*i]
         src_pts[2*i+1]
 
@@ -68,75 +58,50 @@ def main():
     c3 = intercepts[2]
     c4 = intercepts[3]
 
-    HorizonPt1 = ((c1-c2)/(a2-a1),(c1*a2-c2*a1)/(a2-a1))
-    HorizonPt2 = ((c3-c4)/(a4-a3),(c3*a4-c4*a3)/(a4-a3))
+    HorizonPt1 = (int((c1-c2)/(a2-a1)),int((c1*a2-c2*a1)/(a2-a1)))
+    HorizonPt2 = (int((c3-c4)/(a4-a3)),int((c3*a4-c4*a3)/(a4-a3)))
 
-    HorizonSlope = (HorizonPt2[1]-HorizonPt1[1])/(HorizonPt2[0]-HorizonPt1[0])
-    HorizonIntercept = HorizonPt1[1]-HorizonSlope*HorizonPt1[0]
+ 
+    # Create a new image that combines the two input images side by side
+    height1, width1 = img1.shape[:2]
+    nearest = int(min(HorizonPt1[0],HorizonPt2[0],0))
+    furthest = int(max(HorizonPt1[0],HorizonPt2[0],width1))
+    new_width = int(abs(furthest-nearest))
+    new_height = height1
+    new_img = np.zeros((new_height, new_width, 3), dtype=np.uint8)
+    pstart = -nearest
+    #rint(nearest)
+    #print(furthest)
+    #print(new_img.shape)
+    new_img[0:height1, pstart:pstart+width1] = img1
 
-    def draw_correspondences(img1, img2, kp1, kp2, good_matches):
-        
-        # Create a new image that combines the two input images side by side
-        height1, width1 = img1.shape[:2]
-        height2, width2 = img2.shape[:2]
-        new_width = width1 + width2
-        new_height = max(height1, height2)
-        new_img = np.zeros((new_height, new_width, 3), dtype=np.uint8)
-        new_img[:height1, :width1] = img1
-        new_img[:height2, width1:] = img2
+    Graphpoints = []
+    HP1 = (HorizonPt1[0]+pstart,HorizonPt1[1])
+    HP2 = (HorizonPt2[0]+pstart,HorizonPt2[1])
 
-        overlay = new_img.copy()
-        
-        # Draw circles around the keypoints and lines connecting the matched keypoints
-        if good_matches == "M":
-            for i in range(len(kp1)):
-                
-                p1 = kp1[i]
-
-                # Shift the x-coordinate of the second point to account for the offset in the new image
-                p2_shifted = (kp2[i][0] + width1, kp2[i][1])
-
-                # Draw circles around the keypoints and lines connecting the matched keypoints
-                color = tuple(np.random.randint(0, 255, 3).tolist())  # Select a random color for each line
-                cv2.circle(new_img, p1, 5, color, -1)
-                cv2.circle(new_img, p2_shifted, 5, color, -1)
-                cv2.line(new_img, p1, p2_shifted, color, 2)
-        else:
-            for match in good_matches:
-                # Get the keypoints from the good matches
-                kp1_idx = match.queryIdx
-                kp2_idx = match.trainIdx
-                p1 = tuple(map(int, kp1[kp1_idx].pt))
-                p2 = tuple(map(int, kp2[kp2_idx].pt))
-
-                # Shift the x-coordinate of the second point to account for the offset in the new image
-                p2_shifted = (p2[0] + width1, p2[1])
-
-                # Draw circles around the keypoints and lines connecting the matched keypoints
-                color = tuple(np.random.randint(0, 255, 3).tolist())  # Select a random color for each line
-                cv2.circle(new_img, p1, 5, color, -1)
-                cv2.circle(new_img, p2_shifted, 5, color, -1)
-                cv2.line(new_img, p1, p2_shifted, color, 2)
-
-        alpha = 0.40
-        new_img = cv2.addWeighted(overlay, alpha, new_img, 1 - alpha, 0)
-
-        return new_img
+    for item in src_pts:
+        Graphpoints.append((item[0]+pstart,item[1]))
 
 
 
-    dst_pts = np.float32(dst_pts).reshape(-1, 1, 2)
-    src_pts = np.float32(src_pts).reshape(-1, 1, 2)
+    for i in range(4):
+        cv2.line(new_img, HP1, Graphpoints[i], (0, 0, 255), 20)
 
-    print('Number of points:', len(src_pts))
+    for i in range(4):
+        cv2.line(new_img, HP2, Graphpoints[i+4], (255, 0, 0), 20)
 
-    M = cv2.getPerspectiveTransform(src_pts,dst_pts)
+    cv2.line(new_img, HP1, HP2, (0, 255, 0), 20)
 
-    # Generate final image
-    height, width, _ = img1.shape
-    result = cv2.warpPerspective(img1_c, M, (width, height))
+    #src_pts = np.float32(src_pts).reshape(-1, 1, 2)
 
-    cv2.imwrite('images/Q3-'+number+'modified.jpg', result)
+    #print(src_pts)
+    #print(HorizonPt1)
+    #print(HorizonPt2)
+
+    result = new_img
+
+
+    cv2.imwrite('images/Q4-'+number+'Horizon.jpg', result)
 
     print("Finished")
 
